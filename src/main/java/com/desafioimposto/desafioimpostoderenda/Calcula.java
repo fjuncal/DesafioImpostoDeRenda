7495726dad7fc9ef4f36927ba552690ec4e10f71
Fellipe Juncal <fjuncal7@gmail.com>
2020-02-07 23:13:20 -0300

    Criando os métodos calculaInss, salarioDescontadoInss e descontoDependente

--- src/main/java/com/desafioimposto/desafioimpostoderenda/Calcula.java
+++ src/main/java/com/desafioimposto/desafioimpostoderenda/Calcula.java
@@ -4,6 +4,8 @@ package com.desafioimposto.desafioimpostoderenda;
 
 public class Calcula {
     public static void main(String[] args) {
-        
+        Operacoes op = new Operacoes();
+       
+        System.out.println(op.calculaInss(3000));
     }
 }
--- src/main/java/com/desafioimposto/desafioimpostoderenda/Operacoes.java
+++ src/main/java/com/desafioimposto/desafioimpostoderenda/Operacoes.java
@@ -1,4 +1,35 @@
 package com.desafioimposto.desafioimpostoderenda;
 public class Operacoes {
-    
+      public float calculaInss(float salario){
+        float desconto = 0;
+        if (salario <= 1751.81f){
+            desconto = salario * 0.08f;
+            return desconto;
+         } else if ((salario >= 1751.82f) && (salario <= 2919.72f)){
+             desconto = salario * 0.09f;
+             return desconto;  
+         } else if ((salario >= 2919.73f) && (salario <= 5839.45f)){
+             desconto = salario * 0.11f;
+             return desconto;    
+         } else if (salario > 5839.45f){
+             desconto = 642.34f;
+             return desconto;
+         }
+        return desconto;
+    }
+      public float salarioDescontadoInss(float salario, float descontoInss){
+        float novoSalario;
+        
+        novoSalario = salario - descontoInss;
+        return novoSalario;
+    }
+      public float descontoDependente (int numDependente, float salarioInss){
+         float valorDependente;
+         float salarioDescontadoDep;
+         
+         valorDependente = numDependente * 189.59f;
+         salarioDescontadoDep = salarioInss - valorDependente;
+         
+         return salarioDescontadoDep;        
+    }
 }
