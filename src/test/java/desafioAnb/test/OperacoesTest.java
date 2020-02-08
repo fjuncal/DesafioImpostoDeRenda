0c9ce0935ab7c8468b523e8823e11af0982cb9c8
Fellipe Juncal <fjuncal7@gmail.com>
2020-02-08 16:37:39 -0300

    Classe OperacoesTest concluída

--- src/test/java/desafioAnb/test/OperacoesTest.java
+++ src/test/java/desafioAnb/test/OperacoesTest.java
@@ -14,7 +14,7 @@ public class OperacoesTest extends TestCase {
 	int numeroDependentes = 0;
 	
 	float inss = op.calculaInss(salario);
-	float salarioDescontadoInss = salario - inss;
+	float salarioDescontadoInss = op.salarioDescontadoInss(salario, inss);
         float descontoDependente = op.descontoDependente(numeroDependentes, salarioDescontadoInss);
 	float irpf = op.impostoIrpf(salarioDescontadoInss, descontoDependente);
 	float descontoTotal = op.descontoTotal(irpf, inss);
@@ -26,52 +26,39 @@ public class OperacoesTest extends TestCase {
 	assertEquals(920.00f, salarioComDescontos);	
 }
     
-//    public void testCalculaInss(){
-//        float salario;
-//        
-//        salario = op.calculaInss(3000f);
-//        assertEquals(salario, 330.00f);
-//        
-//    }
-//    
-//    public void testSalarioDescontadoInss(){
-//        float novoSalario;
-//        
-//        novoSalario = op.salarioDescontadoInss(3000f, 330f);
-//        assertEquals(novoSalario, 2670.00f);
-//        
-//    }
-//    
-//    public void testDescontoDependente(){
-//        float salarioDescontoDep;
-//        
-//        salarioDescontoDep = op.descontoDependente(2, 2670f);
-//        assertEquals(salarioDescontoDep, 2290.82f);
-//          
-//     }
-//     
-//    public void testImpostoIrpf(){
-//        float calculaIrpf;
-//         
-//        calculaIrpf = op.impostoIrpf(2670.0f, 2290.82f);
-//        assertEquals(calculaIrpf, 29.011505f);
-//        
-//        
-//     }
-//     
-//    public void testDescontoTotal(){
-//         float descontoTotal;
-//         
-//         descontoTotal = op.descontoTotal(29.01f, 330f);
-//         assertEquals(descontoTotal, 359.01f);
-//     }
-//     
-//    public void testSalarioFinal(){
-//         float salarioFinal;
-//         
-//         salarioFinal = op.salarioFinal(3000, 359.01f);
-//         assertEquals(salarioFinal, 2640.99f);
-//     }
+    public void testCasoDeTeste2(){
+        float salario = 3000f;
+        int numeroDependentes = 2;
+        
+        float inss = op.calculaInss(salario);
+	float salarioDescontadoInss = op.salarioDescontadoInss(salario, inss);
+        float descontoDependente = op.descontoDependente(numeroDependentes, salarioDescontadoInss);
+	float irpf = op.impostoIrpf(salarioDescontadoInss, descontoDependente);
+	float descontoTotal = op.descontoTotal(irpf, inss);
+	float salarioComDescontos = op.salarioFinal(salario, descontoTotal);
+        
+        assertEquals(330.00f, inss);
+	assertEquals(29.011505f, irpf);
+	assertEquals(359.0115f, descontoTotal);
+	assertEquals(2640.9885f, salarioComDescontos);
+        
+    }
     
+    public void testCasoDeTeste3(){
+        float salario = 6000f;
+        int numeroDependentes = 0;
+        
+        float inss = op.calculaInss(salario);
+	float salarioDescontadoInss = op.salarioDescontadoInss(salario, inss);
+        float descontoDependente = op.descontoDependente(numeroDependentes, salarioDescontadoInss);
+	float irpf = op.impostoIrpf(salarioDescontadoInss, descontoDependente);
+	float descontoTotal = op.descontoTotal(irpf, inss);
+	float salarioComDescontos = op.salarioFinal(salario, descontoTotal);
+        
+        assertEquals(642.34f, inss);
+	assertEquals(603.9966f, irpf);
+	assertEquals(1246.3367f, descontoTotal);
+	assertEquals(4753.663f, salarioComDescontos);
+    }
 
 }
