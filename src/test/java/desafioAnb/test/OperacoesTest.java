288af34b7fb1338aedbfb4f821c768a6152b2b76
Fellipe Juncal <fjuncal7@gmail.com>
2020-02-08 15:25:35 -0300

    Melhoria da classe OperacoesTest

--- src/test/java/desafioAnb/test/OperacoesTest.java
+++ src/test/java/desafioAnb/test/OperacoesTest.java
@@ -10,35 +10,25 @@ public class OperacoesTest extends TestCase {
     
     public void testCalculaInss(){
         float salario;
-        float desconto;
-        salario = op.calculaInss(3000f);
-        desconto = 330.00f;
         
-        assertEquals(desconto, salario);
+        salario = op.calculaInss(3000f);
+        assertEquals(salario, 330.00f);
         
     }
     
       public void testSalarioDescontadoInss(){
         float novoSalario;
-        float salario = 3000f;
-        float desconto = 330f;
-        novoSalario = op.salarioDescontadoInss(salario, desconto);
         
-       
-          assertEquals(novoSalario, novoSalario);
+        novoSalario = op.salarioDescontadoInss(3000f, 330f);
+        assertEquals(novoSalario, 2670.00f);
         
     }
     
      public void testDescontoDependente(){
-        float salarioInss = 2670f;
-        float valorDependente = 379.18f;
         float salarioDescontoDep;
         
-        
-        salarioDescontoDep = op.descontoDependente(2, salarioInss);
-        
-       
-          assertEquals(salarioDescontoDep, 2290.82f);
+        salarioDescontoDep = op.descontoDependente(2, 2670f);
+        assertEquals(salarioDescontoDep, 2290.82f);
           
      }
      
@@ -46,7 +36,6 @@ public class OperacoesTest extends TestCase {
         float calculaIrpf;
          
         calculaIrpf = op.impostoIrpf(2670.0f, 2290.82f);
-        
         assertEquals(calculaIrpf, 29.011505f);
         
         
@@ -56,7 +45,6 @@ public class OperacoesTest extends TestCase {
          float descontoTotal;
          
          descontoTotal = op.descontoTotal(29.01f, 330f);
-         
          assertEquals(descontoTotal, 359.01f);
      }
      
@@ -64,23 +52,7 @@ public class OperacoesTest extends TestCase {
          float salarioFinal;
          
          salarioFinal = op.salarioFinal(3000, 359.01f);
-         
          assertEquals(salarioFinal, 2640.99f);
      }
     
-//    public OperacoesTest(String testName) {
-//        super(testName);
-//    }
-//    
-//    @Override
-//    protected void setUp() throws Exception {
-//        super.setUp();
-//    }
-//    
-//    @Override
-//    protected void tearDown() throws Exception {
-//        super.tearDown();
-//    }
-
-   
 }
